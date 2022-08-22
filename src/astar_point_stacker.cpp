3f0708a48daf42c7cc9ae2673657bc79d6639566
Andrey <yyootttaa@gmail.com>
2022-08-22 17:03:15 +0300

    Completed astar_point_stacker.
    MAIN GOAL: Check astar trajectory for collisions with obstacles.
    
    """

--- src/astar_point_stacker.cpp
+++ src/astar_point_stacker.cpp
@@ -18,23 +18,45 @@ class AstarPointStacker
     AstarPointStacker()
     {
         goal_index = 0;
-        goal_threshold = 0.1;
+        goal_threshold = 0.5;
         goal_is_sent = 1;
     }
 
     // Функция для получения положения новой цели
-    geometry_msgs::PoseStamped get_next_goal()
+    void get_next_goal()
     {
-        if(goal_index < trajectory_storage.waypoints.capacity())
+//DEBUG rinfo
+//****************************************************************************************************
+if(1)
+{
+    ROS_INFO_STREAM(std::endl << "_________________________________" << std::endl << "_________________________________" << std::endl 
+    << "FUNCTION NAME: get_next_goal()" << std::endl 
+    << "VARIABLES: " << std::endl
+    << "goal_index -->" << goal_index << std::endl 
+    << "trajectory_storage.waypoints.size() -->" << trajectory_storage.waypoints.size() << std::endl 
+    << "goal_is_sent -->" << goal_is_sent << std::endl 
+    << "_________________________________" << std::endl << "_________________________________" << std::endl);
+}
+//**************************************************************************************************** 
+        if(goal_index < trajectory_storage.waypoints.size())
         {
             actual_goal = trajectory_storage.waypoints.at(goal_index);
             goal_is_sent = 0;
+            ROS_INFO_STREAM("1st if");
         }
 
-        if (goal_index < trajectory_storage.waypoints.capacity())
+        if (goal_index < trajectory_storage.waypoints.size())
         {
             goal_index++;
+            ROS_INFO_STREAM("2nd if");
         }
+        
+    }
+
+    // Функция для обнуления индекса
+    void reset_goal_index()
+    {
+        goal_index = 0;
     }
 };
 
@@ -43,41 +65,65 @@ AstarPointStacker astar_point_stacker;
 
 ros::Publisher next_goal_pub;
 
+// Колбэк астара. Получает новую траекторию и записывает ее в класс для хранения
 void astar_cb(const apf_la::GlobalTrajectory::ConstPtr& trajectory)
 {
-    trajectory_storage = *trajectory;
-    // for (int i = 0; i < trajectory->waypoints.capacity(); i++)
-    // {
-    //     // make a copy of the trajectory object in trajectory_storage
-    //     trajectory_storage.waypoints.push_back(trajectory->waypoints.at(i));
-    //     // trajectory_storage.waypoints.resize(trajectory->waypoints.capacity());
-    //     // trajectory_storage.waypoints.at(i).header.stamp = trajectory->waypoints.at(i).header.stamp;
-    //     // trajectory_storage.waypoints.at(i).header.frame_id = trajectory->waypoints.at(i).header.frame_id;
-    //     // trajectory_storage.waypoints.at(i).pose.position.x = trajectory->waypoints.at(i).pose.position.x;
-    //     // trajectory_storage.waypoints.at(i).pose.position.y = trajectory->waypoints.at(i).pose.position.y;
-    //     // trajectory_storage.waypoints.at(i).pose.position.z = trajectory->waypoints.at(i).pose.position.z;
-    //     // trajectory_storage.waypoints.at(i).pose.orientation.x = trajectory->waypoints.at(i).pose.orientation.x;
-    //     // trajectory_storage.waypoints.at(i).pose.orientation.y = trajectory->waypoints.at(i).pose.orientation.y;
-    //     // trajectory_storage.waypoints.at(i).pose.orientation.z = trajectory->waypoints.at(i).pose.orientation.z;
-    //     // trajectory_storage.waypoints.at(i).pose.orientation.w = trajectory->waypoints.at(i).pose.orientation.w;
-    //     trajectory_storage.waypoints.at(i) = trajectory->waypoints.at(i);
-    // }
-    ROS_INFO_STREAM("Received trajectory with " << trajectory->waypoints.capacity() << " waypoints");
+    for (int i = 0; i < trajectory->waypoints.size(); i++)
+    {
+        // make a copy of the trajectory object in trajectory_storage
+        trajectory_storage.waypoints.resize(trajectory->waypoints.size());
+        trajectory_storage.waypoints.at(i).header.stamp = trajectory->waypoints.at(i).header.stamp;
+        trajectory_storage.waypoints.at(i).header.frame_id = trajectory->waypoints.at(i).header.frame_id;
+        trajectory_storage.waypoints.at(i).pose.position.x = trajectory->waypoints.at(i).pose.position.x;
+        trajectory_storage.waypoints.at(i).pose.position.y = trajectory->waypoints.at(i).pose.position.y;
+        trajectory_storage.waypoints.at(i).pose.position.z = trajectory->waypoints.at(i).pose.position.z;
+        trajectory_storage.waypoints.at(i).pose.orientation.x = trajectory->waypoints.at(i).pose.orientation.x;
+        trajectory_storage.waypoints.at(i).pose.orientation.y = trajectory->waypoints.at(i).pose.orientation.y;
+        trajectory_storage.waypoints.at(i).pose.orientation.z = trajectory->waypoints.at(i).pose.orientation.z;
+        trajectory_storage.waypoints.at(i).pose.orientation.w = trajectory->waypoints.at(i).pose.orientation.w;
+    }
+    ROS_INFO_STREAM("Received trajectory with " << trajectory->waypoints.size() << " waypoints");
+    astar_point_stacker.reset_goal_index();
     astar_point_stacker.get_next_goal();
 }
 
+
 void odom_cb(const nav_msgs::Odometry::ConstPtr& odom)
 {
-    if(pow(odom->pose.pose.position.x, 2) + pow(odom->pose.pose.position.y, 2) < pow(astar_point_stacker.goal_threshold, 2))
+//DEBUG rinfo
+//****************************************************************************************************
+if(0)
+{
+    ROS_INFO_STREAM(std::endl << "_________________________________" << std::endl << "_________________________________" << std::endl 
+    << "FUNCTION NAME: odom_cb" << std::endl 
+    << "VARIABLES: " << std::endl 
+    << "odom.x -->" << odom->pose.pose.position.x << std::endl 
+    << "odom.y -->" << odom->pose.pose.position.y << std::endl 
+    << "odom.z -->" << odom->pose.pose.position.z << std::endl 
+    << "odom.orientation.x -->" << odom->pose.pose.orientation.x << std::endl 
+    << "odom.orientation.y -->" << odom->pose.pose.orientation.y << std::endl 
+    << "odom.orientation.z -->" << odom->pose.pose.orientation.z << std::endl 
+    << "odom.orientation.w -->" << odom->pose.pose.orientation.w << std::endl 
+    << "_________________________________" << std::endl << "_________________________________" << std::endl);
+}
+//****************************************************************************************************
+
+// ROS_INFO_STREAM("\n#############################################################\n#############################################################\n########	odom coords:" <<	odom->pose.pose.position.x << " " << odom->pose.pose.position.y <<  " " << odom->pose.pose.position.z	<<	"\n########	goal coords:" << astar_point_stacker.actual_goal.pose.position.x << " "	<< astar_point_stacker.actual_goal.pose.position.y << " " << astar_point_stacker.actual_goal.pose.position.z	<<	"\n#############################################################\n#############################################################");
+ROS_INFO_STREAM("\n#############################################################\n#############################################################\n########	distance to goal:" << 	pow(odom->pose.pose.position.x - astar_point_stacker.actual_goal.pose.position.x, 2) + pow(odom->pose.pose.position.y - astar_point_stacker.actual_goal.pose.position.y, 2)	<< 		"\n########	threshold:" << 	pow(astar_point_stacker.goal_threshold, 2)	<< 		"\n#############################################################\n#############################################################");
+
+    if(pow(odom->pose.pose.position.x - astar_point_stacker.actual_goal.pose.position.x, 2) + pow(odom->pose.pose.position.y - astar_point_stacker.actual_goal.pose.position.y, 2) < pow(astar_point_stacker.goal_threshold, 2))
     {
+        ROS_WARN_STREAM("Debug flag 1");
         astar_point_stacker.get_next_goal();
     }
+
     if (astar_point_stacker.goal_is_sent == 0)
     {
         next_goal_pub.publish(astar_point_stacker.actual_goal);
         astar_point_stacker.goal_is_sent = 1;
     }
 }
+
 
 int main(int argc, char **argv)
 {
