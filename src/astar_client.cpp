ab0ecddfa3f4f533f1ee594a2dd8116c4104b5b6
Andrey <andreylerov.agent@gmail.com>
2022-09-02 02:39:29 +0300

    started reworking odometry trajectory check algo

--- src/astar_client.cpp
+++ src/astar_client.cpp
@@ -7,6 +7,7 @@
 #include "nav_msgs/Odometry.h"
 #include "geometry_msgs/Point.h"
 
+double buffer_life_length = 5.0;
 
 ros::ServiceClient client;
 ros::Publisher trajectory_pub;
@@ -15,7 +16,8 @@ ros::Publisher trajectory_vis_pub;
 
 geometry_msgs::PoseStamped bufferized_goal;
 std::vector<nav_msgs::Odometry> bufferized_odometry;
-int succ = 0;
+int velocity_parameter_pass_counter = 0;
+int odom_parameter_pass_counter = 0;
 
 
 void goal_cb(const geometry_msgs::PoseStamped::ConstPtr& goal)
@@ -51,42 +53,28 @@ void vel_cb(const geometry_msgs::Twist::ConstPtr& velocity)
 {
     if( sqrt(pow(velocity->linear.x, 2) + pow(velocity->linear.y, 2)) < 0.01 && abs(velocity->angular.z < 0.2) )
     {
-        succ++;
+        velocity_parameter_pass_counter++;
     }
 }
 
 
 void odom_cb (const nav_msgs::Odometry::ConstPtr& odom)
 {
-    for (int i = 1; i < bufferized_odometry.size(); i++)
-    {
-        bufferized_odometry.at(i-1) = bufferized_odometry.at(i);
-    }
-    bufferized_odometry.at(bufferized_odometry.size()-1) = *odom;
-
-    if( sqrt(pow(odom->twist.twist.linear.x, 2) + pow(odom->twist.twist.linear.y, 2)) < 0.01 && abs(odom->twist.twist.angular.z < 0.2) )
-    {
-        succ++;
-    }
-
-    // std::vector<geometry_msgs::Point> odom_delta;
-    // odom_delta.resize(bufferized_odometry.size()-1);
-
-    // for(int i = 1; i < bufferized_odometry.size() - 1; i++)
+    // for (int i = 1; i < bufferized_odometry.size(); i++)
     // {
-    //     odom_delta.at(i).x = bufferized_odometry.at(i).pose.pose.position.x - bufferized_odometry.at(i-1).pose.pose.position.x;
-    //     odom_delta.at(i).y = bufferized_odometry.at(i).pose.pose.position.y - bufferized_odometry.at(i-1).pose.pose.position.y;
+    //     bufferized_odometry.at(i-1) = bufferized_odometry.at(i);
     // }
+    // bufferized_odometry.at(bufferized_odometry.size()-1) = *odom;
+    bufferized_odometry.push_back(*odom);
 
-    // for (int i = 1; i < odom_delta.size() - 1; i++)
-    // {
-    //     if (sqrt(pow(odom_delta.at(i).x, 2) + pow(odom_delta.at(i).y, 2)) - sqrt(pow(odom_delta.at(i-1).x, 2) + pow(odom_delta.at(i-1).y, 2)) > 0.001)
-    //     {
-    //         succ++;
-    //     }
-    // }
+    // int j = 0;
+    while (bufferized_odometry.at(0).header.stamp.toSec() < ros::Time::now().toSec() - buffer_life_length)
+    {
+        bufferized_odometry.erase(bufferized_odometry.begin());
+        odom_parameter_pass_counter++;
+    }
 
-    if(succ >= 25)
+    if (odom_parameter_pass_counter >= 150)
     {
         apf_la::GetTrajectory srv;
         srv.request.goal_position.pose.position.x = bufferized_goal.pose.position.x;
@@ -102,7 +90,53 @@ void odom_cb (const nav_msgs::Odometry::ConstPtr& odom)
             }
             trajectory_pub.publish(trajectory);
             ROS_INFO_STREAM("Published trajectory with" << srv.response.path.size() << "waypoints\n");
-            succ = 0;
+            velocity_parameter_pass_counter = 0;
+        }
+
+        else
+        {
+            ROS_ERROR("Failed to call service.");
+        }
+    }
+    // if( sqrt(pow(odom->twist.twist.linear.x, 2) + pow(odom->twist.twist.linear.y, 2)) < 0.01 && abs(odom->twist.twist.angular.z < 0.2) )
+    // {
+    //     velocity_parameter_pass_counter++;
+    // }
+
+    // std::vector<geometry_msgs::Point> odom_delta;
+    // odom_delta.resize(bufferized_odometry.size()-1);
+
+    // for(int i = 1; i < bufferized_odometry.size() - 1; i++)
+    // {
+    //     odom_delta.at(i).x = bufferized_odometry.at(i).pose.pose.position.x - bufferized_odometry.at(i-1).pose.pose.position.x;
+    //     odom_delta.at(i).y = bufferized_odometry.at(i).pose.pose.position.y - bufferized_odometry.at(i-1).pose.pose.position.y;
+    // }
+
+    // for (int i = 1; i < odom_delta.size() - 1; i++)
+    // {
+    //     if (sqrt(pow(odom_delta.at(i).x, 2) + pow(odom_delta.at(i).y, 2)) - sqrt(pow(odom_delta.at(i-1).x, 2) + pow(odom_delta.at(i-1).y, 2)) > 0.001)
+    //     {
+    //         velocity_parameter_pass_counter++;
+    //     }
+    // }
+
+    if(velocity_parameter_pass_counter >= 25)
+    {
+        apf_la::GetTrajectory srv;
+        srv.request.goal_position.pose.position.x = bufferized_goal.pose.position.x;
+        srv.request.goal_position.pose.position.y = bufferized_goal.pose.position.y;
+        ROS_ERROR_STREAM("Stuck! Resending goal...\n");
+
+        if (client.call(srv))
+        {
+            apf_la::GlobalTrajectory trajectory;
+            for(int i = 0; i < srv.response.path.size(); i++)
+            {
+                trajectory.waypoints.push_back(srv.response.path.at(i));
+            }
+            trajectory_pub.publish(trajectory);
+            ROS_INFO_STREAM("Published trajectory with" << srv.response.path.size() << "waypoints\n");
+            velocity_parameter_pass_counter = 0;
         }
 
         else
@@ -124,7 +158,7 @@ int main(int argc, char **argv)
     ros::Subscriber odom_sub = n.subscribe("/odom", 100, odom_cb);
     trajectory_pub = n.advertise<apf_la::GlobalTrajectory>("/astar/trajectory", 10);
 
-    bufferized_odometry.resize(10);
+    // bufferized_odometry.resize(10);
 
     ros::spin();
     return 0;
